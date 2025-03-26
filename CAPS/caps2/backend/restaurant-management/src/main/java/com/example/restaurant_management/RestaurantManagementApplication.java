41b31d2efbb3445a4793bb2c1becad7246577e07
Akshat3128 <akshatsaxena62941@gmail.com>
2025-03-27 02:21:52 +0530

    reg and login api done caps2

--- CAPS/caps2/backend/restaurant-management/src/main/java/com/example/restaurant_management/RestaurantManagementApplication.java
+++ CAPS/caps2/backend/restaurant-management/src/main/java/com/example/restaurant_management/RestaurantManagementApplication.java
@@ -2,12 +2,21 @@ package com.example.restaurant_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class RestaurantManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestaurantManagementApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(RestaurantManagementApplication.class);
 
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(RestaurantManagementApplication.class, args);
+            logger.info(" Server started successfully on port 8080!");
+            logger.info(" Database connected successfully!");
+        } catch (Exception e) {
+            logger.error(" Error starting server: ", e);
+        }
+    }
 }
